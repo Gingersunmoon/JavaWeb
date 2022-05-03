5d6720efc30b9a03f221b9e412116c770fdd8e0f
Gingersunmoon <1160625388@qq.com>
2022-05-03 17:44:49 +0800

    练习

--- GITTest/src/GitHunExercise.java
+++ GITTest/src/GitHunExercise.java
@@ -1,5 +1,8 @@
 public class GitHunExercise {
     public static void main(String[] args) {
-        System.out.println("hello java!");
+        int[] sum=new int[]{3,5,6};
+        sum[0]=8;
+        System.out.println(sum[0]+sum[1]);
+        Integer.parseInt("jiangke");
     }
 }
